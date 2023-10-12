171f520c51efdfe8573794b134ad19fd362e756d
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-10-12 00:10:15 -0300

    feat: add command handling

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,68 @@ impl Todo {
         self.completed = true;
     }
 
-    fn uncomplete(&mut self) {
-        self.completed = false;
-    }
-
     fn to_string(&self) -> String {
-        format!("{} - Completed: {}", self.description, self.completed)
+        format!("{}", self.description)
     }
 }
 
 
 fn main() {
-    let mut todo = Todo::new("Commit code".to_string());
-    println!("Todo: {}", todo.to_string());
-    todo.complete();
-    println!("Todo: {}", todo.to_string());
-    todo.uncomplete();
-    println!("Todo: {}", todo.to_string());
+    let args: Vec<String> = std::env::args().collect();
+
+    let command = args[1].clone();
+
+    if command == "list" {
+        let current_todos: Vec<Todo> = std::fs::read_to_string("todos")
+            .expect("Something went wrong reading the file")
+            .lines()
+            .map(|line| Todo::new(line.to_string()))
+            .collect();
+
+        for (index, todo) in current_todos.iter().enumerate() {
+            println!("{} -> {}", index, todo.to_string());
+        }
+    }
+
+    if command == "done" {
+        let mut current_todos: Vec<Todo> = std::fs::read_to_string("todos")
+            .expect("Something went wrong reading the file")
+            .lines()
+            .map(|line| Todo::new(line.to_string()))
+            .collect();
+
+        let done_index: usize = args[2].parse().expect("Error parsing index");
+
+        current_todos[done_index].complete();
+
+        let mut file_content = String::new();
+
+        for todo in current_todos.iter() {
+            file_content.push_str(&todo.to_string());
+            file_content.push_str("\n");
+        }
+
+        std::fs::write("todos", file_content).expect("Error writing file");
+    }
+
+    if command == "new" {
+        let mut current_todos: Vec<Todo> = std::fs::read_to_string("todos")
+            .expect("Something went wrong reading the file")
+            .lines()
+            .map(|line| Todo::new(line.to_string()))
+            .collect();
+
+        let new_todo = Todo::new(args[2].clone());
+
+        current_todos.push(new_todo);
+
+        let mut file_content = String::new();
+
+        for todo in current_todos.iter() {
+            file_content.push_str(&todo.to_string());
+            file_content.push_str("\n");
+        }
+
+        std::fs::write("todos", file_content).expect("Error writing file");
+    }
 }
